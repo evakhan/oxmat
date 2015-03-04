4497888cf0d5a68725baa97b3a1633372f07a025
Riaz Uddin <masum@activelava.net>
2015-03-04 18:10:33 +0600

    some change

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
+    
     'use strict';
+    
     $(".search").click(function () {
         $(".formwork").toggleClass("formview");
+    });
+    
+     $(function () {
+      $('[data-toggle="tooltip"]').tooltip();
     });
 });
